6382baf45584203b662807aaad341e2d3b7e5ef0
Eoin Crosbie <eoin.crosbie@feedhenry.com>
2015-01-16 11:18:02 -0600

    support sending msg

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var env=require("./env");
 var eventBus=require("./eventbus");
 var init=require("./init");
-module.exports=function(option){
+var stompClient = require("./jms/stompClient");
+
+
+exports.startClient=function(option){
   for (var key in option){
     env.set(key,option[key]);
   }
   init();
   return eventBus;
-}
+};
+
+exports.sendMessage = function(params,cb){
+    var queueName = params.queueName || process.env.out_queue;
+    var message = params.message || '<Empty message>';
+    var result = stompClient.publish(queueName,message);
+
+    if(result){
+        return cb(null,"message posted");
+    } else {
+        return cb("message posting failed");
+    }
+
+};
