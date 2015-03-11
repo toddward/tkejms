45c83ed6c1da700d81331940ae0226f2f941a378
Eoin Crosbie <eoin.crosbie@feedhenry.com>
2015-03-11 12:05:23 +0000

    fix typo

--- jms/stompClient.js
+++ jms/stompClient.js
@@ -8,7 +8,7 @@ function AMQService() {}
 
 AMQService.prototype.log = require("../log");
 AMQService.prototype.init = function(cb) {
-  self.logger.info('AMQ service init called');
+  self.log.info('AMQ service init called');
   var self = this;
   if (client === null) {
     this.log.info("Start to initialise AMQService");
